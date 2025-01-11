5dfb3df23139e27f8489dc4ac4fdec882aadcf3a
SanellaPalangic <sanella.palangic@gmail.com>
2025-01-10 21:27:08 -0500

    Add files via upload

---
+++ computerPlayer.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "UserPlayer.h"
+
+#include <vector>
+
+class Board;
+class computerPlayer :
+	public Player
+{
+private:
+	std::vector<int> m_RowPositions;
+	std::vector<int> m_ColPositions;
+
+
+	int m_AvailablePositions = 0;
+
+
+	int selectSpawnPos();
+	void searchPositions(Board& board);
+
+public:
+	void getPosition(Board& board, int& row, int& col);
+};
---
+++ humanPlayer.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Player.h"
+
+#include <string>
+
+class Board;
+class humanPlayer :
+	public Player
+{
+public:
+
+	int getPosition(const std::string direction, int size) const;
+};
